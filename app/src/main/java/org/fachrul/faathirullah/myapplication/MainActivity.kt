91a314d1ac48d1d486e78ab083a96c390aa7c24b
Fachrul <ffaathirullah1@gmail.com>
2020-08-17 05:52:27 +0700

    Add EditText, Done Button, ClickHandler

--- app/src/main/java/org/fachrul/faathirullah/myapplication/MainActivity.kt
+++ app/src/main/java/org/fachrul/faathirullah/myapplication/MainActivity.kt
@@ -1,12 +1,38 @@
 package org.fachrul.faathirullah.myapplication
 
+
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethod
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btn_done).setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+
+    private fun addNickname(view: View){
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        val inm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
